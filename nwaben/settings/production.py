839bcb442af1d141f570324b311200ad10bed494
Gerard-007 <Gerardnwazk@gmail.com>
2020-08-07 23:54:54 -0700

    added tawkto plugnin and mp3 models

--- nwaben/settings/production.py
+++ nwaben/settings/production.py
@@ -218,16 +218,3 @@ LOGOUT_REDIRECT_URL = 'home'
 
 # ACCOUNT_EMAIL_REQUIRED = True
 # ACCOUNT_AUTHENTICATION_METHOD = "username_email"
-
-
-# #======DJANGO-audiofield settings=====
-# # Frontend widget values
-# # 0-Keep original, 1-Mono, 2-Stereo
-# CHANNEL_TYPE_VALUE = 0
-#
-# # 0-Keep original, 8000-8000Hz, 16000-16000Hz, 22050-22050Hz,
-# # 44100-44100Hz, 48000-48000Hz, 96000-96000Hz
-# FREQ_TYPE_VALUE = 8000
-#
-# # 0-Keep original, 1-Convert to MP3, 2-Convert to WAV, 3-Convert to OGG
-# CONVERT_TYPE_VALUE = 0
